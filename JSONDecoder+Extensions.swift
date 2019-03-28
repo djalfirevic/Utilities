248d8f5f295b48172bc355752f224b1a580ff3fa
Djuro Alfirevic <djalfirevic@gmail.com>
2019-03-28 20:03:13 +0100

    JSONDecoder+Extensions.swift added

---
+++ JSONDecoder+Extensions.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+extension JSONDecoder {
+    
+    func decode<T: Decodable>(_ type: T.Type, fromURL url: String, completion: @escaping (T) -> Void) {
+        guard let url = URL(string: url) else { fatalError("Invalid URL passed.") }
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                let downloadedFriends = try self.decode(type, from:
+                    data)
+
+                DispatchQueue.main.async {
+                    completion(downloadedFriends)
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
+
